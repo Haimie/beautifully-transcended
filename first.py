89abe1fec08b044cfb912da8b8202895a60c8104
Haimie Getenga <haimiegetenga1@gmail.com>
2023-01-20 07:54:15 +0300

    arithmetic operators

---
+++ first.py
@@ -0,0 +1,20 @@
+# addition
+print('2+3:',2+3)
+#multiplication
+print('23*6:',23*6)
+#division
+print('2/3:',2/3)
+#floor division
+print('3.5//2:',3.5//2)
+#square root
+print('square root of 49:', 49**0.5)
+#remainder
+print('the remainder of 5/2:', 5%2)
+#subtraction
+print('6-2:',6-2)
+
+
+x=9
+ans=x**2+3*x+5
+print('polynomial:', ans)
+
